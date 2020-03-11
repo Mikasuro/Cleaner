cff172f07c4034e6f07a1d335cbc0d9c06dfeec6
Максим Шиян <maksim.shiyann@mail.ru>
2020-03-11 22:30:23 +0700

    commit

---
+++ Cleaner/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Cleaner.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Cleaner
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private List<DiskInfo> _diskInfos;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public List<DiskInfo> DiskInfos
+        {
+            get => _diskInfos;
+            set
+            {
+                _diskInfos = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DiskInfos = DriveInfo.GetDrives().Select(s => new DiskInfo()).ToList();
+            DataContext = this;
+        }
+    }
+}
---
+++ Cleaner/Model/DiskInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaner.Model
+{
+    public class DiskInfo
+    {
+        public string Name { get; set; }
+      
+        public DriveType DriveType { get; set; }
+        
+        public long TotalFreeSpace { get; set; }
+     
+        public int TotalSize { get; set; }
+
+        public int TotalSizeGb { get { return TotalSize / (1024 * 1024 * 1024); } }
+        
+        public string VolumeLabel { get; set; }
+    }
+}
